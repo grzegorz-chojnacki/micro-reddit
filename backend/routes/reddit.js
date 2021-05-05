edf646241e42b54fe80c754eb1faa11762607d51
Grzegorz Chojnacki <grzegorzc60@gmail.com>
2021-05-05 18:14:17 +0200

    Added routes for specific reddits

--- backend/routes/reddit.js
+++ backend/routes/reddit.js
@@ -3,19 +3,32 @@ const router = express.Router();
 
 router.route("/")
   .get((req, res) => {
-    // const query = req.query.q || "";
-    // const page = req.query.p  || 0;
-    res.json([
-      { id: 1, name: "A", text: "aaa" },
-      { id: 2, name: "B", text: "bbb" },
-      { id: 3, name: "C", text: "ccc" },
-      { id: 4, name: "D", text: "ddd" },
-      { id: 5, name: "E", text: "eee" },
-    ]);
+    const query = req.query.q || "";
+    const page = req.query.p  || 0;
+    res.json({
+      query, page, reddits: [
+        { id: 1, name: "A", text: "aaa" },
+        { id: 2, name: "B", text: "bbb" },
+        { id: 3, name: "C", text: "ccc" },
+        { id: 4, name: "D", text: "ddd" },
+        { id: 5, name: "E", text: "eee" },
+      ]
+    });
   })
   .post((req, res) => {
     const reddit = req.body
     res.json({...reddit, id: 1000 });
+  });
+
+router.route("/:reddit")
+  .get((req, res) => {
+    const redditId = req.params.reddit
+    res.json({ id: redditId, name: "Z", text: "zzz" })
+  })
+  .put((req, res) => {
+    const oldReddit = { id: 12, name: "X", text: "xxx" }
+    const newReddit = req.body
+    res.json({ ...oldReddit, ...newReddit })
   });
 
 router.use((_, res) => res.status(400).json('Bad Request'));

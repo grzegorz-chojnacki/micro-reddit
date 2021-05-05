80cd4af0c7a326778a642478ff4ae6848e8e92bf
Grzegorz Chojnacki <grzegorzc60@gmail.com>
2021-05-05 18:23:48 +0200

    Added posts methods

--- backend/routes/reddit.js
+++ backend/routes/reddit.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const router = express.Router();
 
+const pagination = req => ({
+  query: req.query.q || "",
+  page: req.query.p  || 0
+});
+
 router.route("/")
   .get((req, res) => {
-    const query = req.query.q || "";
-    const page = req.query.p  || 0;
+    const { query, page } = pagination(req)
+
     res.json({
       query, page, reddits: [
         { id: 1, name: "A", text: "aaa" },
@@ -30,6 +35,26 @@ router.route("/:reddit")
     const newReddit = req.body
     res.json({ ...oldReddit, ...newReddit })
   });
+
+router.route("/:reddit/p")
+  .get((req, res) => {
+    const { query, page } = pagination(req)
+    const redditId = req.params.reddit
+
+    res.json({
+      redditId, query, page,
+      posts: [
+        { id: 1, name: "Aaa", text: "Aaa aaa", comments: [{}] },
+        { id: 2, name: "Bbb", text: "Bbb bbb", comments: [{}] },
+        { id: 3, name: "Ccc", text: "Ccc ccc", comments: [{}] },
+      ]
+    });
+  })
+  .post((req, res) => {
+    const post = req.body
+    res.json({ ...post, id: 2000 });
+  });
+
 
 router.use((_, res) => res.status(400).json('Bad Request'));
 

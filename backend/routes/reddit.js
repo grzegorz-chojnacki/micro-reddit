f1a556a2fb05cd9abc26ace80ef1184fb7bd63ab
Grzegorz Chojnacki <grzegorzc60@gmail.com>
2021-05-05 18:07:10 +0200

    Added create-reddit method

--- backend/routes/reddit.js
+++ backend/routes/reddit.js
@@ -1,18 +1,22 @@
 const express = require("express");
 const router = express.Router();
 
-router.route("/").get((req, res) => {
-  // const query = req.query.q || "";
-  // const page = req.query.p  || 0;
-
-  res.json([
-    { id: 1, name: "A", text: "aaa" },
-    { id: 2, name: "B", text: "bbb" },
-    { id: 3, name: "C", text: "ccc" },
-    { id: 4, name: "D", text: "ddd" },
-    { id: 5, name: "E", text: "eee" },
-  ]);
-});
+router.route("/")
+  .get((req, res) => {
+    // const query = req.query.q || "";
+    // const page = req.query.p  || 0;
+    res.json([
+      { id: 1, name: "A", text: "aaa" },
+      { id: 2, name: "B", text: "bbb" },
+      { id: 3, name: "C", text: "ccc" },
+      { id: 4, name: "D", text: "ddd" },
+      { id: 5, name: "E", text: "eee" },
+    ]);
+  })
+  .post((req, res) => {
+    const reddit = req.body
+    res.json({...reddit, id: 1000 });
+  });
 
 router.use((_, res) => res.status(400).json('Bad Request'));
 

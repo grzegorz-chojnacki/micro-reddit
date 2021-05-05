7f165d7f475481c4f9462e3969c11e95933a777f
Grzegorz Chojnacki <grzegorzc60@gmail.com>
2021-05-05 21:02:19 +0200

    Added input data validators

--- backend/utils.js
+++ backend/utils.js
@@ -1,3 +1,43 @@
+const v = {
+  isNumber:     x => typeof x === "number" && x !== NaN,
+  isString:     x => typeof x === "string",
+  isObject:     x => typeof x === "object",
+  defined:      x => x !== undefined,
+  required:     x => v.isNumber(x) || !!x,
+  requiredText: x => v.isString(x) && v.required(x),
+
+  isPassword: x => v.isString(x) && x.length > 8,
+  isId:       x => v.isString(x) && v.required(x),
+  isEmail:    x => v.requiredText(X) && x.includes('@') && x.includes('.'),
+  isUrl:      x => v.requiredText(X) && x.includes('.'),
+  hasOptions: x => typeof x.options === 'object' && x.options instanceof Array
+    && x.options.length > 1 && x.options.every(v.requiredText),
+
+  isImage:  x => !v.defined(x) || v.requiredText(x),
+  isSurvey: x => !v.defined(x) || v.isObject(x) && v.hasOptions(x),
+  isvideo:  x => !v.defined(x) || v.isUrl(x) && (x.includes('youtube') || x.includes('youtu.be')),
+
+  isNewReddit: x => v.requiredText(x.name),
+  isAnswer:    x => v.isObject(x) && v.isNumber(x.answer),
+
+  isNewUser: x => {
+    const { username, password, email } = x;
+    return v.requiredText(username) && v.isPassword(password) && v.isEmail(email);
+  },
+
+  isNewPost: x => {
+    const { name, text, image, video, survey } = x;
+    return v.requiredText(name) && v.requiredText(text)
+      && v.isImage(image) && v.isvideo(video) && v.isSurvey(survey);
+  },
+
+  isComment: x => {
+    const { user, text } = x;
+    return v.isObject(user) && v.isId(user.id) && v.requiredText(text);
+  }
+}
+
 module.exports = {
   pagination: req => ({ query: req.query.q || "", page: req.query.p  || 0 }),
+  validator: v,
 }

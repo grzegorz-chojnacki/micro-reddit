2fc8283bba5f3b2f500fa2c4c2f83c0337e539b1
Grzegorz Chojnacki <grzegorzc60@gmail.com>
2021-05-23 18:04:21 +0200

    Added reddit list searching

--- frontend/src/views/RedditListView.vue
+++ frontend/src/views/RedditListView.vue
@@ -1,5 +1,12 @@
 <template>
   <main>
+    <h2 v-if="query" class="my-3">
+      <button class="btn" @click="clearQuery">
+        <span class="material-icons clear">clear</span>
+      </button>
+      Results for "{{ query }}":
+    </h2>
+
     <Reddit class="my-2"
       v-for="reddit of reddits" :reddit="reddit" :key="reddit.id"/>
   </main>
@@ -18,12 +25,12 @@ export default {
   data() {
     return {
       page: 0,
-      search: '',
+      query: '',
       reddits: [],
       sourceExhausted: false
     }
   },
-  created() {
+  mounted() {
     this.fetchNext()
     window.onscroll = () => {
       const scrollHeight = window.pageYOffset + window.innerHeight
@@ -34,9 +41,19 @@ export default {
       }
     }
   },
+  watch: {
+    $route() { this.reset() }
+  },
   methods: {
+    reset() {
+      this.reddits = []
+      this.page = 0
+      this.fetchNext()
+    },
+    clearQuery() { this.$router.push({ name: 'reddit-list' }); },
     fetchNext() {
-      redditService.getAll(this.page, this.search).then(reddits => {
+      this.query = this.$route.query.q || ''
+      redditService.getAll(this.page, this.query).then(reddits => {
         this.reddits = this.reddits.concat(reddits)
         this.page++
         this.sourceExhausted = reddits.length === 0
@@ -45,3 +62,11 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+h2 > button {
+  display: inline-flex;
+  align-items: center;
+  padding: 0.5em;
+}
+</style>

ced57b7c38fd68faf4fab5194c8faa5411bce1e2
Grzegorz Chojnacki <grzegorzc60@gmail.com>
2021-05-26 21:50:11 +0200

    Added Subject and isAuthenticated Observable

--- frontend/src/common.js
+++ frontend/src/common.js
@@ -1,1 +1,22 @@
 export const api = 'http://localhost:8080/api'
+
+export const Subject = value => {
+  let observers = []
+  return {
+    asObservable: () => ({
+      subscribe: observer => {
+        observers = [...observers, observer]
+        observer(value)
+        return {
+          unsubscribe: () => {
+            observers = observers.filter(o => o !== observer)
+          }
+        }
+      },
+    }),
+    next: newValue => {
+      value = newValue
+      observers.forEach(o => o(value))
+    }
+  }
+}

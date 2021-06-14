180a4980c69cfd5955fca30b2d8a7e4dcd2f7bf1
Grzegorz Chojnacki <grzegorzc60@gmail.com>
2021-06-14 21:13:33 +0200

    Added subscription method to service

--- frontend/src/services/redditService.js
+++ frontend/src/services/redditService.js
@@ -6,23 +6,33 @@ export const redditService = {
   async getAll(page = 0, query = "") {
     return (await api.get(`/r?p=${page}&q=${query}`)).data;
   },
+
   async add(name, text) {
     return (await api.post("/r", { name, text })).data.id;
   },
+
   async update(reddit) {
     const { data } = (await api.put(`/r/${reddit.id}`, reddit));
     redditSource.next(data);
     return data;
   },
+
   async get(redditId) {
     const { data } = await api.get(`/r/${redditId}`);
     redditSource.next(data);
     return data;
   },
+
+  async setSubscribe(redditId, state = false) {
+    await api.patch(`/u/r/${redditId}`, { state });
+    redditSource.next({ ...redditSource.value, subscribed: state });
+  },
+
   async addMod(redditId, username) {
     await api.post(`/r/${redditId}/m/${username}`);
     this.get(redditId);
   },
+
   get reddit() {
     return redditSource.asObservable();
   },

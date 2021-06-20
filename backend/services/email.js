b5cdce6e5f69bb33966aa5e79c32cca83eaad246
Grzegorz Chojnacki <grzegorzc60@gmail.com>
2021-06-20 16:54:25 +0200

    Added commented ethereal setup as a backup

--- backend/services/email.js
+++ backend/services/email.js
@@ -23,3 +23,28 @@ module.exports = ({
     transporter.close();
   }
 });
+
+// module.exports = ({
+//   async remindPassword({ nickname, email, password }) {
+//     const testAccount = await nodemailer.createTestAccount();
+
+//     const transporter = nodemailer.createTransport({
+//       host: "smtp.ethereal.email",
+//       port: 587,
+//       secure: false, // true for 465, false for other ports
+//       auth: {
+//         user: testAccount.user, // generated ethereal user
+//         pass: testAccount.pass, // generated ethereal password
+//       },
+//     });
+
+//     const info = await transporter.sendMail({
+//       from: "\"MicroReddit Support\" <no-reply@microreddit.com>",
+//       to: `${email}`,
+//       subject: "Password reminder",
+//       text: `Hello ${nickname}! Your password is ${password}`
+//     });
+
+//     console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+//   }
+// });

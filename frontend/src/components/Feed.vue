6fd2858509f560a2e9ee7f759d485373ce3496f4
Grzegorz Chojnacki <grzegorzc60@gmail.com>
2021-06-20 14:29:08 +0200

    Added another post component

--- frontend/src/components/Feed.vue
+++ frontend/src/components/Feed.vue
@@ -1,14 +1,41 @@
 <template>
-  <div>
-    <h2 v-if="query" class="my-3">
+  <div class="my-3">
+    <h2 v-if="query">
       <router-link :to="{ name: 'reddit' }" class="text-reset">
         <span class="material-icons clear">clear</span>
       </router-link>
       Results for "{{ queryStr }}":
     </h2>
 
+    <div class="form-check form-switch ms-1">
+      <input
+        id="postComponent"
+        v-model="showCards"
+        type="checkbox"
+        class="form-check-input">
+      <label class="form-check-label" for="postComponent">Display cards</label>
+    </div>
+
+
     <section>
-      <Post v-for="post of posts" :key="post.id" :post="post" @delete="onDelete" />
+      <template v-if="showCards">
+        <Post
+          v-for="post of posts"
+          :key="post.id"
+          :post="post"
+          @vote="post.score = $event.score"
+          @delete="onDelete" />
+      </template>
+      <template v-else>
+        <ul class="list-group list-group-flush">
+          <PostMinimal
+            v-for="post of posts"
+            :key="post.id"
+            :post="post"
+            @vote="post.score = $event.score"
+            @delete="onDelete" />
+        </ul>
+      </template>
     </section>
 
     <footer><LoadingIndicator :done="sourceExhausted" /></footer>
@@ -17,16 +44,18 @@
 
 <script>
 import Post from "@/components/Post.vue";
+import PostMinimal from "@/components/PostMinimal.vue";
 import LoadingIndicator from "@/components/LoadingIndicator.vue";
 import { userService } from "@/services/userService.js";
 import { Range, atPageBottom } from "@/common.js";
 
 export default {
   name: "Feed",
-  components: { Post, LoadingIndicator },
+  components: { Post, PostMinimal, LoadingIndicator },
   props: { fetchingFn: { type: Function, required: true } },
   data() {
     return {
+      showCards: true,
       subscription: null,
       page: 0,
       query: this.$route.query.q || "",
@@ -57,7 +86,6 @@ export default {
   },
   unmounted() {
     this.subscription.unsubscribe();
-    window.onscroll = null;
   },
   methods: {
     fetchNext() {
--- frontend/src/components/PostMinimal.vue
+++ frontend/src/components/PostMinimal.vue
@@ -1,8 +1,8 @@
 <template>
-  <section class="card">
-    <header class="card-header">
+  <li class="list-group-item py-3">
+    <header>
       <div class="d-flex justify-content-between">
-        <h5 class="card-title">
+        <h5>
           <router-link
             :to="{
               name: 'post',
@@ -17,7 +17,7 @@
         </button>
       </div>
 
-      <h6 class="card-subtitle">
+      <h6>
         <span class="post-author">
           Posted by <strong>{{ post.user?.username }}</strong> at
         </span>
@@ -26,61 +26,22 @@
           {{ post.reddit.name }}
         </router-link>
       </h6>
+      <span class="post-score"> with score of {{ post.score }}</span>
     </header>
-
-    <div class="card-body">
-      <!-- <iframe
-        v-if="post.video"
-        :src="embedYoutube(post.video)"
-        class="yt-player"
-        type="text/html"
-        frameborder="0" /> -->
-
-      <div v-if="post.image" class="img-container rounded-2 bg-dark mb-3">
-        <img :src="imageUrl" :alt="post.title">
-      </div>
-
-      <p v-if="post.link" class="card-text">
-        <a :href="post.link">
-          {{ post.link }}
-        </a>
-      </p>
-
-      <p v-if="post.content" class="card-text">
-        {{ post.content }}
-      </p>
-    </div>
-
-    <footer class="card-footer">
-      <VoteGroup :score="score" :voted="voted" @vote="onVote" />
-
-      <router-link
-        class="btn mx-3"
-        :to="`/r/${post.reddit.name}/p/${post.id}#comments`">
-        Comments
-      </router-link>
-    </footer>
-  </section>
+  </li>
 </template>
 
 <script>
-import VoteGroup from "@/components/VoteGroup.vue";
 import { postService } from "@/services/postService.js";
 import { userService } from "@/services/userService.js";
-import { getYoutubeVideoId, baseURL } from "@/common.js";
-
-const urlMapper = url => url.match(/^http.*/) ? url : `${baseURL}/s/${url}`;
 
 export default {
   name: "Post",
-  components: { VoteGroup },
   props: { post: { type: Object, required: true } },
   emits: ["delete", "vote"],
   data() {
     return {
-      imageUrl: this.post.image ? urlMapper(this.post.image) : "",
       score: this.post.score,
-      voted: this.post.voted,
       modView: userService.isMod(this.post.reddit.name)
     };
   },
@@ -91,9 +52,6 @@ export default {
     }
   },
   methods: {
-    embedYoutube(url) {
-      return `https://www.youtube.com/embed/${getYoutubeVideoId(url)}`;
-    },
     async onVote(state) {
       const { score } = await postService.vote(this.post.reddit.name, this.post.id, state);
       this.score = score;
@@ -109,23 +67,7 @@ export default {
 </script>
 
 <style scoped lang="scss">
-section {
-  margin: 1em 0;
-}
-
-.img-container {
-  overflow: hidden;
-  max-height: 800px;
-
-  img {
-    box-shadow: 0 0 1em black;
-    display: block;
-    max-width: 100%;
-    margin: 0 auto;
-  }
-}
-
-.post-author { opacity: 0.7 }
+.post-author, .post-score { opacity: 0.7 }
 
 header {
   & > * {
@@ -135,10 +77,5 @@ header {
       overflow-wrap: anywhere;
     }
   }
-}
-
-.yt-player {
-  width: 100%;
-  height: 500px;
 }
 </style>
